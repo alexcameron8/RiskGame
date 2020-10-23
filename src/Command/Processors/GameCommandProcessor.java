70b25ba4a21ba0383d8b1a01ad7fc2162de1b84f
Ben Munro <ben.munro2000@gmail.com>
2020-10-23 17:54:49 -0400

    Added help menus.

--- src/Command/Processors/GameCommandProcessor.java
+++ src/Command/Processors/GameCommandProcessor.java
@@ -19,6 +19,13 @@ public class GameCommandProcessor extends CommandProcessor{
                 System.out.print(player.getName() + " ");
             }
             System.out.print("\n");
+        } else if(commandWord.equals("help")) {
+            System.out.println("Help:");
+            System.out.println("You are currently in a game of Risk: Global Domination.");
+            System.out.println("Available Commands:");
+            System.out.println("(players) List player names.");
+            System.out.println("(quit) Quits the game.");
+
         } else if(commandWord.equals("quit")){
             game.setState(GameState.QUIT);
         }
