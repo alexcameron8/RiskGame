93aa011f83461ed76774d7048d52bd9b2a1460b5
Alex Cameron <alexjcameron@cmail.carleton.ca>
2020-10-23 15:13:23 -0400

    Player class before commands

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -1,4 +1,27 @@
 package Player;
+import java.util.*;
+import Map.*;
 
 public class Player {
+    private ArrayList<Territory> listOfTerritories;
+    private ArrayList<String> listOfContinents;
+    private String name;
+
+    public Player(String name){
+        this.name = name;
+        this.listOfContinents = new ArrayList<>();
+        this.listOfTerritories = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+    public ArrayList<String> getListOfContinents() {
+        return listOfContinents;
+    }
+
+    public ArrayList<Territory> getListOfTerritories() {
+        return listOfTerritories;
+    }
+
 }
