5952b9b8720e2e99f502886280520ffc32a744e0
Alex Cameron <alexjcameron@cmail.carleton.ca>
2020-10-24 19:58:02 -0400

    Added javaDocs to Risk Class

--- src/Main/Risk.java
+++ src/Main/Risk.java
@@ -12,6 +12,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
 
+/**
+ * This is the Risk Class.
+ */
 public class Risk {
     private static GameState state;
     private Parser parser;
@@ -21,7 +24,9 @@ public class Risk {
     private Turn currentTurn;
 
 
-
+    /**
+     * This class creates a new Risk game.
+     */
     Risk(){
         parser = new Parser(this);
         players = new ArrayList<Player>();
@@ -29,6 +34,9 @@ public class Risk {
         map = new WorldMap();
     }
 
+    /**
+     * the printMenu method prints all possible commands when in the opening menu when launching the game.
+     */
     private void printMenu(){
         System.out.println("Welcome to Main.Risk: Global Domination!");
         System.out.println("To get started, please select and option:");
@@ -37,6 +45,9 @@ public class Risk {
         System.out.println("(quit) Quit");
     }
 
+    /**
+     * This method is used to advance the turns in the auto setup phase.
+     */
     public void advanceAutoTurn() {
         if (activePlayerID + 1 < players.size()) {
             activePlayerID++;
@@ -46,7 +57,7 @@ public class Risk {
     }
 
     /**
-     *
+     * This method creates a new Turn instance and changes which player is currently playing the game.
      */
     public void advanceTurn(){
         if(currentTurn.isTurnComplete()){
@@ -80,30 +91,60 @@ public class Risk {
         activePlayerID = 0;
     }
 
+    /**
+     * This method accesses the currentTurn.
+     * @return The current Turn
+     */
     public Turn getActivePlayerTurn(){
         return currentTurn;
 
     }
+
+    /**
+     * This method gets the current player whos turn it is.
+     * @return The current player who's turn it is.
+     */
     public Player getActivePlayer(){
         return players.get(activePlayerID);
     }
 
+    /**
+     * This method returns what state the game is (e.g IN_GAME , MAIN_MENU ...)
+     * @return The state the game is in
+     */
     public GameState getState(){
         return state;
     }
 
+    /**
+     * This method sets the state the game is in. (e.g IN_GAME, MAIN_MENU ..)
+     * @param state the state to set the game
+     */
     public void setState(GameState state){
         this.state = state;
     }
 
+    /**
+     * This method returns the list of players in the game.
+     * @return the list of players
+     */
     public ArrayList<Player> getPlayers(){
         return players;
     }
 
+    /**
+     * This method adds a new player to the game of Risk.
+     * @param newPlayer the new player being added to the game
+     */
     public void addPlayer(Player newPlayer){
         players.add(newPlayer);
     }
 
+    /**
+     * This method returns the name of the specified player
+     * @param name the name of the player
+     * @return the specified player
+     */
     public Player getPlayerByName(String name){
         for (Player player: players) {
             if(player.getName().equals(name)){
@@ -114,10 +155,19 @@ public class Risk {
         return null;
     }
 
+    /**
+     * This returns the map in the game.
+     *
+     * @return The map of game of Risk
+     */
     public WorldMap getMap(){
         return this.map;
     }
 
+    /**
+     * This method processes all potential commands entered when trying to perform actions in Risk.
+     * @param command The command being processed
+     */
     private void processCommand(Command command){
         CommandProcessor cp = null;
         if(!command.isValid()){
@@ -140,6 +190,11 @@ public class Risk {
         }
     }
 
+    /**
+     * Getter method returning number of troops each player has at beginning of game based on how many players
+     * there are.
+     * @return The number of troops each player has
+     */
     private int getNumberOfInitialTroops(){
         switch (players.size()){
             case 2:
@@ -157,6 +212,9 @@ public class Risk {
         }
     }
 
+    /**
+     * This method is the auto-setup functionality of Risk where the players are
+     */
     public void assignTroopsRandom(){
         ArrayList<Territory> territories = map.getWorldMap().getTerritories();
         Collections.shuffle(territories);
@@ -180,6 +238,9 @@ public class Risk {
 
     }
 
+    /**
+     * This method plays the game and changes the game state and initializes the setup of the game.
+     */
     public void play(){
         state = GameState.MAIN_MENU;
         printMenu();
