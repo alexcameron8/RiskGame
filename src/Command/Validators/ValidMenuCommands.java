8a7774e2b6555d7b7cd812928c77a984cb564d26
Alex Cameron <alexjcameron@cmail.carleton.ca>
2020-10-24 13:12:03 -0400

    Java docs

--- src/Command/Validators/ValidMenuCommands.java
+++ src/Command/Validators/ValidMenuCommands.java
@@ -2,9 +2,16 @@ package Command.Validators;
 
 import java.util.ArrayList;
 
+/**
+ * This is a class of all valid menu commands.
+ */
 public class ValidMenuCommands implements ValidCommands{
     private static ArrayList<String> validCommands;
 
+    /**
+     * The constructor returns an ArrayList of all the valid Strings pertaining to all valid commands accessible in the
+     * menu.
+     */
     public ValidMenuCommands(){
         validCommands = new ArrayList<String>();
         validCommands.add("start");
