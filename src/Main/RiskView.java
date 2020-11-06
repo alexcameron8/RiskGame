f749b0a874d3e257aba9a48b6a4f0291ebcc0e70
Alex Cameron <alexjcameron@cmail.carleton.ca>
2020-11-06 16:36:56 -0500

    Added Welcome Screen

--- src/Main/RiskView.java
+++ src/Main/RiskView.java
@@ -1,10 +1,24 @@
 package Main;
 
+import Main.ActionBar.ActionBarView;
+import Main.PlayerBar.PlayerBarView;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
-public class RiskGame extends JFrame {
-    RiskGame(){
+public class RiskView extends JFrame {
+    private RiskModel riskModel;
+    private RiskController riskController;
+
+    RiskView(){
+        this.riskModel = new RiskModel();
+        this.riskController = new RiskController(riskModel);
+
+        initSetup();
+
         this.setSize(new Dimension(800, 600));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       this.setJMenuBar(new MenuBarView());
@@ -12,7 +26,7 @@ public class RiskGame extends JFrame {
         MapView mapView = new MapView();
         TerritoryInfoView territoryInfoView = new TerritoryInfoView();
        mapView.getMapModel().addMapListener(territoryInfoView);
-        
+
         this.add(new ActionBarView(), BorderLayout.PAGE_START);
         this.add(mapView, BorderLayout.CENTER);
         this.add(new TerritoryInfoView(), BorderLayout.LINE_END);
@@ -20,8 +34,22 @@ public class RiskGame extends JFrame {
 
         this.setVisible(true);
     }
+    public void initSetup(){
 
+        ImageIcon icon = new ImageIcon("src/Main/Resources/Risk.png");
+        JLabel labelIcon = new JLabel(icon);
+        JPanel panel = new JPanel(new GridBagLayout());
+        panel.add(labelIcon);
+        JLabel text = new JLabel("Welcome to Risk. Click OK to continue to game setup.");
+        text.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        text.setFont(new Font("Ubuntu", Font.BOLD, 20));
+        JPanel panel2 = new JPanel();
+        panel2.setLayout(new BoxLayout(panel2, BoxLayout.PAGE_AXIS));
+        panel2.add(panel);
+        panel2.add(text);
+        JOptionPane.showMessageDialog(this,panel2, "Welcome to Risk",JOptionPane.DEFAULT_OPTION);
+    }
     public static void main(String[] args) {
-        new RiskGame();
+        new RiskView();
     }
 }
