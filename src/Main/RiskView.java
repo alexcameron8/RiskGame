6f84e4f3eefa6b0c94893445cfaa46bc1a3e277f
Thomas <thomasdunnigan@cmail.carleton.ca>
2020-11-06 16:43:57 -0500

    Fixed problems

--- src/Main/RiskView.java
+++ src/Main/RiskView.java
@@ -1,11 +1,17 @@
 package Main;
 
+import Main.ActionBar.*;
+import Main.PlayerBar.*;
 import javax.swing.*;
 import java.awt.*;
 
-public class RiskGame extends JFrame {
-    RiskGame(){
+public class RiskView extends JFrame {
+    RiskView(){
         this.setSize(new Dimension(800, 600));
+
+        RiskModel rm = new RiskModel();
+        rm.addRiskView(this);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       this.setJMenuBar(new MenuBarView());
 
@@ -15,13 +21,13 @@ public class RiskGame extends JFrame {
         
         this.add(new ActionBarView(), BorderLayout.PAGE_START);
         this.add(mapView, BorderLayout.CENTER);
-        this.add(new TerritoryInfoView(), BorderLayout.LINE_END);
+        this.add(territoryInfoView, BorderLayout.LINE_END);
         this.add(new PlayerBarView(), BorderLayout.PAGE_END);
 
         this.setVisible(true);
     }
 
     public static void main(String[] args) {
-        new RiskGame();
+        new RiskView();
     }
 }
