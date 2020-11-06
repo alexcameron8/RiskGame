b3c58ee811331d6bd905ba621bff687d61f2e78b
Alex Cameron <alexjcameron@cmail.carleton.ca>
2020-11-06 13:20:25 -0500

    Created base players list with dummy data

--- src/Main/PlayerBarView.java
+++ src/Main/PlayerBarView.java
@@ -1,13 +1,49 @@
 package Main;
 
+import Player.Player;
+
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.util.ArrayList;
 
 public class PlayerBarView extends JPanel {
+
+    private Color darkBlue = new Color(102,178,255);
+    private JList<Player> playersList;
+    private Player[] players = {new Player("Alex"), new Player("Ben"), new Player("Thomas")};
+
     PlayerBarView(){
         // JPanel Config
-        this.setLayout(new BorderLayout());
-        this.setBackground(Color.MAGENTA);
-        this.add(new JLabel("Player Bar"), BorderLayout.CENTER);
+        this.setLayout(new FlowLayout(FlowLayout.LEFT));
+        this.setBackground(darkBlue);
+        initPlayerPanel();
+        initPlayersList();
     }
+
+    public void initPlayerPanel(){
+        JLabel playersPanel = new JLabel("Players:");
+        playersPanel.setBackground(darkBlue);
+        this.add(playersPanel);
+    }
+    public void initPlayersList(){
+        playersList = new JList<>(players);
+        playersList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+        playersList.setVisibleRowCount(1);
+        playersList.setOpaque(false);
+        playersList.setCellRenderer(new TransparentListCellRenderer());
+        this.add(playersList);
+    }
+
+    public class TransparentListCellRenderer extends DefaultListCellRenderer {
+
+        @Override
+        public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+            super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+            setOpaque(isSelected);
+            return this;
+        }
+
+    }
+
 }
