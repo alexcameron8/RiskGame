ae5a95b0c46c0c900816dd6876eac3acec776a63
Ben Munro <ben.munro2000@gmail.com>
2020-11-07 16:24:46 -0500

    Added some docs

--- src/Main/MapView.java
+++ src/Main/MapView.java
@@ -10,11 +10,18 @@ import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 
+/**
+ * Map View component. Displays the selectable game map to the user.
+ */
 public class MapView extends JPanel implements MapViewListener{
     private MapModel mapModel;
     private MapController mapController;
     private DrawMap drawMap;
 
+    /**
+     * Create a new MapView
+     * @param riskModel Game model containing map to represent.
+     */
     MapView(RiskModel riskModel){
         this.mapModel = new MapModel(riskModel);
         this.mapModel.addMapListener(this);
@@ -40,20 +47,34 @@ public class MapView extends JPanel implements MapViewListener{
 
     }
 
+    /**
+     * Get the model of the game map.
+     * @return Model of the game map.
+     */
     MapModel getMapModel(){
         return this.mapModel;
     }
 
+    /**
+     * Handle events from MapModel
+     * @param e MapModel event.
+     */
     @Override
     public void handleMapUpdate(MapEvent e) {
         MapView.this.repaint();
         MapView.this.revalidate();
     }
 
+    /**
+     * Map component
+     */
     private class DrawMap extends JComponent {
         AffineTransform tx;
         Double scale;
 
+        /**
+         * Create new DrawMap
+         */
         DrawMap(){
             super();
             tx = new AffineTransform();
@@ -61,6 +82,11 @@ public class MapView extends JPanel implements MapViewListener{
             tx.scale(scale, scale);
         }
 
+        /**
+         * Handle mouse click events on the map.
+         * Creates a new action containing the ID of the territory clicked.
+         * @param point
+         */
         void mouseClickHandler(Point2D point){
             AffineTransform tx2 = new AffineTransform();
             Double scale2 = (double) this.getWidth() /600;
