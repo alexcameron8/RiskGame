150b53fdb74f28fb6524ccef86411ba19899f0b0
Thomas <thomasdunnigan@cmail.carleton.ca>
2020-11-08 15:02:35 -0500

    fixed territory view

--- src/Main/RiskView.java
+++ src/Main/RiskView.java
@@ -31,8 +31,9 @@ public class RiskView extends JFrame {
         }
         riskModel.play();
 
-        this.setSize(new Dimension(800, 600));
         this.setVisible(true);
+        this.setSize(new Dimension(800,600));
+        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         riskModel.addRiskView(this);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
